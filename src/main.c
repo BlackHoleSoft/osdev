d44b46f088699113da485b8de00cdb23897d82ec
Anton Kanashov <antonkanashov@mail.ru>
2023-12-06 12:03:45 +0300

    fix

--- src/main.c
+++ src/main.c
@@ -4,6 +4,16 @@ int main() {
     printf("Strelka system\n");
     printf("Hello from C with static stdlib");
 
+    char buff[10];
+    int i = 0;
+
+
+    while (i < 10 || buff[i] = scanf("%c", &buf[i]) != '\n') {
+        printf("%c", buf[i]);
+    }
+
+    printf("%10s", buff);
+
     while(1) {
         // loop
     }
