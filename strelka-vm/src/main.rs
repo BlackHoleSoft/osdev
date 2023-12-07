e8de20b9a890820b714748cfb68558a2d4e6223f
anton <antonkanashov@mail.ru>
2023-12-07 19:24:50 +0000

    add bootloader

--- strelka-vm/src/main.rs
+++ strelka-vm/src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static GREETINGS: &[u8] = b"Strelka system";
+
 #[no_mangle]
-pub extern "C" fn start() -> ! {
+pub extern "C" fn _start() -> ! {
+    let vga_buf = 0xb8000 as *mut u8;
+
+    for (i, &byte) in GREETINGS.iter().enumerate() {
+        unsafe {
+            *vga_buf.offset(i as isize * 2 + 0) = byte;
+            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
