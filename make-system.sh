28f7e862f4a43e544548789ae3a981e456bfa3b4
Anton Kanashov <antonkanashov@mail.ru>
2023-12-21 11:38:00 +0300

    fix

--- make-system.sh
+++ make-system.sh
@@ -1,23 +1,39 @@
 #!/bin/sh
 # Run in alpine linux
 # Before running install node and npm (via nvs) and clone this repo to ~/osdev
+# Place busybox src into /mnt/src/busybox
+
+#wget -nc -O kernel.tar.xz http://kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.xz
+#wget -nc -O busybox.tar.bz2 http://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2
+
+#tar -xvf kernel.tar.xz
+#tar -xvf busybox.tar.bz2
 
 cd ./node-strelka
 npm install
 npm run build
 
 mkfs.ext4 /dev/sdb1
-mount /dev/sdb1 /mnt
+mkdir /mnt/system
+mount /dev/sdb1 /mnt/system
 
 mkdir ~/rootfs
+
+cd /mnt/src/busybox
+make defconfig
+LDFLAGS="--static" make busybox install
+cd _install
+cp -r ./ ~/rootfs/
+
 cd ~/rootfs
+rm -f linuxrc
 mkdir -p bin dev mnt proc sys tmp sbin lib
 
 echo '#!/bin/sh' > init
 #echo 'dmesg -n 1' >> init
-#echo 'mount -t devtmpfs none /dev' >> init
-#echo 'mount -t proc none /proc' >> init
-#echo 'mount -t sysfs none /sys' >> init
+echo 'mount -t devtmpfs none /dev' >> init
+echo 'mount -t proc none /proc' >> init
+echo 'mount -t sysfs none /sys' >> init
 echo 'mknod /dev/console c 5 1' >> init
 echo 'mknod /dev/null c 1 3' >> init
 echo 'mknod /dev/fb0 c 29 0' >> init
@@ -36,8 +52,8 @@ chmod +x ./sbin/init
 
 echo 'console.log("Test script running..."); while (true) {}' > ./test.js
 
-mkdir -p ./sys/class/graphics/fb0
-echo '720,400' > ./sys/class/graphics/fb0/virtual_size
+#mkdir -p ./sys/class/graphics/fb0
+#echo '720,400' > ./sys/class/graphics/fb0/virtual_size
 
 cp /bin/sh ./bin
 cp /bin/busybox ./bin
@@ -71,6 +87,7 @@ cp /boot/vmlinuz-lts .
 
 grub-install --boot-directory=/mnt/boot /dev/sdb
 
+# set gfxpayload=1024x768x16
 echo 'set timeout=10' > grub/grub.cfg
 echo 'menuentry "Strelka (node based)" {' >> grub/grub.cfg
 echo '  linux /boot/vmlinuz-lts root=/dev/sda1 rw' >> grub/grub.cfg
