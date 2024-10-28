d6c7cfc858adae4343dfece6999c51d77f1fce62
Anton Kanashov <kanashov.a@aetp.ru>
2024-10-28 13:29:58 +0300

    debian-based image

---
+++ demo_multistrap.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+#
+TARGETDIR=/var/lib/container-example/example2
+
+if ! type apt-get >/dev/null; then
+    echo ERROR: this script only runs on Debian-based systems
+    exit 1
+fi
+
+if sudo find "$TARGETDIR" -maxdepth 1 >/dev/null 2>&1; then
+    echo ERROR: $TARGETDIR exists - refusing to overwrite it
+    echo If you still wish to continue, remove that directory and rerun
+    exit 1
+fi
+
+set -e
+sudo apt-get install multistrap
+
+# Ensure we are in the right directory so that references to the
+# multistrap.conf and multistrap.configscrip both work
+SCRIPTDIR=$(dirname "$0")
+cd "$SCRIPTDIR"
+
+# Note:
+# - the multistrap package install process will start some services.  This can
+#   be avoided by using an appropriate /usr/sbin/policy-rc.d script
+
+set -x
+
+# First run the install stage
+sudo /usr/sbin/multistrap --arch amd64 \
+    -f multistrap.conf \
+    -d $TARGETDIR
+
+# Then run the configure stage
+sudo chroot $TARGETDIR ./multistrap.configscript
+
+# Clean up
+sudo rm $TARGETDIR/multistrap.configscript
